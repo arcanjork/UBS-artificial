4859e23abe0918efe2646c3349eb5e7f70cb4e4b
arcanjork <145770373+arcanjork@users.noreply.github.com>
2025-06-26 14:53:54 -0300

    Delete deepseek_javascript_20250626_d1d8d6.js

--- deepseek_javascript_20250626_d1d8d6.js
+++
@@ -1,9 +0,0 @@
-// Configuração básica para web
-var firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "medsus-app.firebaseapp.com",
-  projectId: "medsus-app",
-  storageBucket: "medsus-app.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
-};
